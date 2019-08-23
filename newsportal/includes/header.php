117f1807d7fd85b69ddb516fbc795f03292b42b8
etekumoses <etekemoses@gmail.com>
2019-08-23 16:27:34 +0300

    newsportal, first commit

---
+++ newsportal/includes/header.php
@@ -0,0 +1,36 @@
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <div class="topbar-left">
+                    <a href="index.php" class="logo"><img src="images/icon.png" style="width: 50px;height: 30px;"><span>SHAMOS<span>CODERS</span></span><i class="mdi mdi-layers"></i></a>
+                    
+                </div>
+
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about-us.php">About</a>
+            </li>
+                 <li class="nav-item">
+              <a class="nav-link" href="index.php">News</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sourcecodes</a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">
+                <a class="dropdown-item" href="#">PHP</a>
+                <a class="dropdown-item" href="#">JAVA</a>
+                <a class="dropdown-item" href="#">HTML</a>
+              </div>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link" href="contact-us.php">Contact us</a>
+            </li>  
+          </ul>
+        </div>
+      </div>
+    </nav>
